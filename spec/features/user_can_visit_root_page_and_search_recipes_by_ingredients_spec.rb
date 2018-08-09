a2ffc0ee164973d9bbcc80f4627ec2fb6372aa1a
Kelley Jenkins <ckjenkins3@gmail.com>
2018-08-09 14:06:23 -0400

    creates home route, view, feature test

---
+++ spec/features/user_can_visit_root_page_and_search_recipes_by_ingredients_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+feature "User can search recipes by ingredients" do
+  scenario "and see list of top 10 recipes with those ingredients" do
+      visit "/"
+      fill_in(:q1, :with => "apples")
+      fill_in(:q2, :with => "sugar")
+      fill_in(:q3, :with => "flour")
+      click_on "Find Recipes"
+
+      expect(current_path).to eq('/')
+      expect(current_url).to include("allowedIngredient[]=apples")
+      expect(current_url).to include("allowedIngredient[]=sugar")
+      expect(current_url).to include("allowedIngredient[]=flour")
+
+      expect(page).to have_content("10 Matching Recipes")
+
+      within(first(".recipes")) do
+        expect(page).to have_css(".name")
+        expect(page).to have_css(".ingredients")
+        expect(page).to have_css(".course-type")
+        expect(page).to have_css(".time")
+      end
+  end
+end
